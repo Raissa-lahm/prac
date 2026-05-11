b527d062546b9081b4afa040622319d5ca0dff6b
Raissa-Lahm <raissalahm25@gmail.com>
2026-05-11 16:06:55 -0300

    mapa interativo inicializado

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
 
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
+// MONGODB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB conectado");
@@ -17,6 +20,17 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(erro);
 });
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
+// FRONTEND
+const frontendPath = path.join(__dirname, "../frontend");
+
+app.use(express.static(frontendPath));
+
+// ROTA PRINCIPAL
+app.get("/", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+// SERVIDOR
+app.listen(4000, () => {
+    console.log("Servidor rodando na porta 4000");
 });
